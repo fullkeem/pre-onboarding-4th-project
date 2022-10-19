e2fc9109adda5c3c636a0e8662563d424ba8486e
choongmankim <cm730712@gmail.com>
2022-10-20 02:54:34 +0900

    Modify: base url 추가

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
   base: '/pre-onboarding-4th-project/',
   plugins: [react()],
-=======
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
->>>>>>> parent of 99ef0ad (Add: dist)
 });
