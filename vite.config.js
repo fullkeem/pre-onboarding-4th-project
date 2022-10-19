0683c8fce93d4a505d6cfef12d0eaf7475a3de34
choongmankim <cm730712@gmail.com>
2022-10-20 02:04:27 +0900

    Add: url 수정

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/pre-onboarding-4th-project',
+  base: '/pre-onboarding-4th-project/',
   plugins: [react()],
   server: {
     port: 3000,
