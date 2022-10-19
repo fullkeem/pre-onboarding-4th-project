7d337bb170feb05f5bab06ede79f648f8c7cd73f
choongmankim <cm730712@gmail.com>
2022-10-20 02:53:27 +0900

    Revert "Add: dist"
    This reverts commit 99ef0ade68c85f9fe3dc212d4cd22f206a4c0ece.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+<<<<<<< HEAD
   base: '/pre-onboarding-4th-project/',
   plugins: [react()],
+=======
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
+>>>>>>> parent of 99ef0ad (Add: dist)
 });
